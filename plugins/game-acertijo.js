f500701ef1bd83b8fb831bd5857c76630c0ebc91
Ayoublkdh <162133258+Ayoublkdh@users.noreply.github.com>
2024-03-15 06:46:25 +0000

    Update game-acertijo.js

--- plugins/game-acertijo.js
+++ plugins/game-acertijo.js
@@ -32,7 +32,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 
 handler.help = ['acertijo']
 handler.tags = ['game']
-handler.command = /^(acertijo|acert|pregunta|adivinanza|tekateki)$/i
+handler.command = /^acertijo|riddle$/i
 handler.register = true
 
 export default handler
